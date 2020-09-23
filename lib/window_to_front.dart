668bf34593b1e2cd05ebff637c0797ea55e221cb
Thomas Erhel <thomas.erhel@gmail.com>
2020-09-23 15:08:07 +0200

    first commit

---
+++ lib/window_to_front.dart
@@ -0,0 +1,10 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+class WindowToFront {
+  static const MethodChannel _channel = const MethodChannel('window_to_front');
+  static Future<void> activate() async {
+    await _channel.invokeMethod('activate');
+  }
+}
